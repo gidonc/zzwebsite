09abe334361855b60ec897a80355822e39e645b4
gidonc <gidon.cohen@durham.ac.uk>
2020-05-27 22:05:24 +0100

    commit from Rstudio

--- content/project/common_project_code.R
+++ content/project/common_project_code.R
@@ -33,18 +33,16 @@ if(readdata==TRUE){
   gs_results <- read_sheet("1VfIqaB--8Y-pF06zp1KEBrTJyX1A_33XKG30n38Da14", sheet=4, col_types="c", skip = 3) %>%
     mutate(team="GS")
   
-  gt_matches <- read_sheet("1oGTfeDSjWoqHaOZYrw09kMxP42bo_sBBVVKJbEThsMI", sheet=3) %>%
-    mutate(teamname="GT")
-  gt_results <- read_sheet("1oGTfeDSjWoqHaOZYrw09kMxP42bo_sBBVVKJbEThsMI", sheet=4, col_types="c", skip = 3) %>%
-    mutate(team="GT")
-  
+  gt_gts_matches <- read_sheet("1oGTfeDSjWoqHaOZYrw09kMxP42bo_sBBVVKJbEThsMI", sheet=3) %>%
+    mutate(teamname=ifelse(match_number < 71, "GT", "GTS"))
+  gt_gts_results <- read_sheet("1oGTfeDSjWoqHaOZYrw09kMxP42bo_sBBVVKJbEThsMI", sheet=4, col_types="c", skip = 3) 
   readr::write_rds(zz_matches, paste0(rprojroot::find_rstudio_root_file(), "/resources/zz_matches.RDS"))
   readr::write_rds(zz_results, paste0(rprojroot::find_rstudio_root_file(), "/resources/zz_results.RDS"))
   readr::write_rds(zz_oldscores, paste0(rprojroot::find_rstudio_root_file(), "/resources/zz_oldscores.RDS"))
   readr::write_rds(gs_matches, paste0(rprojroot::find_rstudio_root_file(), "/resources/gs_matches.RDS"))
   readr::write_rds(gs_results, paste0(rprojroot::find_rstudio_root_file(), "/resources/gs_results.RDS"))
-  readr::write_rds(gt_matches, paste0(rprojroot::find_rstudio_root_file(), "/resources/gt_matches.RDS"))
-  readr::write_rds(gt_results, paste0(rprojroot::find_rstudio_root_file(), "/resources/gt_results.RDS"))
+  readr::write_rds(gt_gts_matches, paste0(rprojroot::find_rstudio_root_file(), "/resources/gt_gts_matches.RDS"))
+  readr::write_rds(gt_gts_results, paste0(rprojroot::find_rstudio_root_file(), "/resources/gt_gts_results.RDS"))
   readr::write_rds(transfers, paste0(rprojroot::find_rstudio_root_file(), "/resources/transfers.RDS"))
 } else {
   zz_matches <- readr::read_rds(paste0(rprojroot::find_rstudio_root_file(), "/resources/zz_matches.RDS"))
@@ -52,8 +50,8 @@ if(readdata==TRUE){
   zz_oldscores <- readr::read_rds(paste0(rprojroot::find_rstudio_root_file(), "/resources/zz_oldscores.RDS"))
   gs_matches <- readr::read_rds(paste0(rprojroot::find_rstudio_root_file(), "/resources/gs_matches.RDS"))
   gs_results <- readr::read_rds(paste0(rprojroot::find_rstudio_root_file(), "/resources/gs_results.RDS"))
-  gt_matches <- readr::read_rds(paste0(rprojroot::find_rstudio_root_file(), "/resources/gt_matches.RDS"))
-  gt_results <- readr::read_rds(paste0(rprojroot::find_rstudio_root_file(), "/resources/gt_results.RDS"))
+  gt_gts_matches <- readr::read_rds(paste0(rprojroot::find_rstudio_root_file(), "/resources/gt_gts_matches.RDS"))
+  gt_gts_results <- readr::read_rds(paste0(rprojroot::find_rstudio_root_file(), "/resources/gt_gts_results.RDS"))
   transfers <- readr::read_rds(paste0(rprojroot::find_rstudio_root_file(), "/resources/transfers.RDS"))
 }
 
@@ -91,14 +89,35 @@ zz_res_long <- zz_results %>%
   inner_join(zz_matches, by="event_round") %>%
   mutate(matches_ago = max(match_number) + 1 - match_number)
 
-gt_results <- gt_results %>%
-  mutate(PlayerTeam=paste0(Players, "(", team, ")")) 
+common_cols <- c("Players", "GP", "Current", "Destination")
+
+gt_matches <- gt_gts_matches %>% filter(teamname=="GT")
+gts_matches <- gt_gts_matches %>% filter(teamname=="GTS")
+gt_cols <- c(common_cols,  gt_matches %>% pull(event_round))
+gts_cols <- c(common_cols, gts_matches %>% pull(event_round))
+gt_results <- gt_gts_results %>%
+  dplyr::select(one_of(gt_cols)) %>%
+  mutate(team="GT", PlayerTeam=paste0(Players, "(", team, ")")) 
+
+gts_results <- gt_gts_results %>%
+  dplyr::select(one_of(gts_cols)) %>%
+  mutate(team="GTS", PlayerTeam=paste0(Players, "(", team, ")")) 
 
 gt_matches <- gt_matches %>%
   mutate(event_round=tolower(event_round),
          event_std = tolower(event))
 
+gts_matches <- gts_matches %>%
+  mutate(event_round=tolower(event_round),
+         event_std = tolower(event))
+
 gt_res_long <- gt_results %>%
+  pivot_longer(-c(Players, PlayerTeam, GP, Current, team), names_to="event_round", values_to = "score") %>%
+  mutate(event_round=tolower(event_round))%>%
+  inner_join(gt_matches, by="event_round") %>%
+  mutate(matches_ago = max(match_number) + 1 - match_number)
+
+gts_res_long <- gts_results %>%
   pivot_longer(-c(Players, PlayerTeam, GP, Current, team), names_to="event_round", values_to = "score") %>%
   mutate(event_round=tolower(event_round))%>%
   inner_join(gt_matches, by="event_round") %>%
@@ -120,12 +139,33 @@ gs_res_long <- gs_results %>%
   inner_join(gs_matches, by="event_round") %>%
   mutate(matches_ago = max(match_number) + 1 - match_number)
 
-results <- bind_rows(zz_results, gs_results, gt_results) %>%
+# results <- bind_rows(zz_results, gs_results, gt_results) %>%
+#   mutate(
+#     safeplayername = myclean(fs::path_sanitize(PlayerTeam)),
+#     playerprofileurl = paste0("playerprofiles/", safeplayername, "/"))
+
+results <- bind_rows(zz_results, gts_results) %>%
   mutate(
     safeplayername = myclean(fs::path_sanitize(PlayerTeam)),
     playerprofileurl = paste0("playerprofiles/", safeplayername, "/"))
 
-res_long <- bind_rows(zz_res_long, gs_res_long, gt_res_long) %>%
+# res_long <- bind_rows(zz_res_long, gs_res_long, gt_res_long) %>%
+#   group_by(event_round) %>%
+#   mutate(     
+#     participated = ifelse(is.na(score), NA_real_, ifelse(score=="NP", 0, 1)),
+#     score = ifelse(is.na(participated), NA_real_, ifelse(participated==1, score, 0)),
+#     safeplayername = myclean(fs::path_sanitize(PlayerTeam)),
+#     GP=as.numeric(GP),
+#     std_GP = (GP-mean(GP, na.rm=TRUE))/sd(GP, na.rm=TRUE),
+#     score = as.numeric(score),
+#     event_sd = sd(score, na.rm=TRUE), 
+#     event_mu = mean(score, na.rm=TRUE),
+#     std_score = (score-event_mu)/event_sd,
+#     has_zig_zag = sum(team=="Zig-Zag Racers"&!is.na(score), na.rm=TRUE)>0,
+#     has_gt = sum(team=="GT"&!is.na(score), na.rm=TRUE)>0,
+#     has_gs = sum(team=="GS"&!is.na(score), na.rm=TRUE)>0)
+
+res_long <- bind_rows(zz_res_long, gts_res_long) %>%
   group_by(event_round) %>%
   mutate(     
     participated = ifelse(is.na(score), NA_real_, ifelse(score=="NP", 0, 1)),
@@ -138,8 +178,7 @@ res_long <- bind_rows(zz_res_long, gs_res_long, gt_res_long) %>%
     event_mu = mean(score, na.rm=TRUE),
     std_score = (score-event_mu)/event_sd,
     has_zig_zag = sum(team=="Zig-Zag Racers"&!is.na(score), na.rm=TRUE)>0,
-    has_gt = sum(team=="GT"&!is.na(score), na.rm=TRUE)>0,
-    has_gs = sum(team=="GS"&!is.na(score), na.rm=TRUE)>0)
+    has_gts = sum(team=="GTS"&!is.na(score), na.rm=TRUE)>0)
 
 
 gp_sd = sd(as.numeric(results$GP), na.rm=TRUE)
@@ -150,10 +189,11 @@ score_mu = mean(as.numeric(res_long$score), na.rm=TRUE)
 most.recent.gs <- gs_res_long %>% filter(!is.na(score)) %>% arrange(-match_number) %>% slice(1) 
 most.recent.zz1 <- zz_res_long %>% filter(!is.na(score)) %>% arrange(-match_number) %>% slice(1) 
 most.recent.gt <- gt_res_long %>% filter(!is.na(score)) %>% arrange(-match_number) %>% slice(1) 
+most.recent.gts <- gts_res_long %>% filter(!is.na(score)) %>% arrange(-match_number) %>% slice(1) 
 
-f1 <- lmer(std_score~ has_zig_zag + has_gs + has_gt + (1|PlayerTeam), res_long)
+f1 <- lmer(std_score~ has_zig_zag + has_gts + (1|PlayerTeam), res_long)
 res_long_recent <- filter(res_long, matches_ago<16)
-f2 <- lmer(std_score~ has_zig_zag + has_gs + has_gt + (1|PlayerTeam), res_long_recent)
+f2 <- lmer(std_score~ has_zig_zag + has_gts + (1|PlayerTeam), res_long_recent)
 
 
 zz_oldscores <- zz_oldscores %>% 
@@ -260,6 +300,14 @@ gt_events <- gt_matches %>%
   ungroup() %>%
   mutate(gt_event_id = row_number())
 
+gts_events <- gts_matches %>%
+  dplyr::rename(gts_event=event) %>%
+  group_by(gts_event, event_std) %>%
+  summarize(gts_no = min(match_number)) %>%
+  arrange(gts_no) %>%
+  ungroup() %>%
+  mutate(gt_event_id = row_number())
+
 zz_events <- all_zz_matches %>%
   dplyr::rename(zz_event=event) %>%
   group_by(zz_event, event_std) %>%
@@ -269,15 +317,16 @@ zz_events <- all_zz_matches %>%
   mutate(zz_event_id = row_number())
 
 events <- dplyr::full_join(gs_events, zz_events, by="event_std") %>%
+  dplyr::full_join(gts_events, by="event_std") %>%
   dplyr::full_join(gt_events, by="event_std") %>%
-  arrange(zz_no, gs_no, gt_no) %>%
+  arrange(zz_no, gts_no, gs_no, gt_no) %>%
   mutate(event_no = row_number())
 
 all_zz_matches <-  all_zz_matches%>% left_join(events %>% dplyr::select(event_std, event_no), by="event_std")
-gt_matches <- gt_matches %>% left_join(events %>% dplyr::select(event_std, event_no), by="event_std") 
+gt_matches <- gt_matches %>% left_join(events %>% dplyr::select(event_std, event_no), by="event_std") gts_matches <- gts_matches %>% left_join(events %>% dplyr::select(event_std, event_no), by="event_std") 
 gs_matches <- gs_matches %>% left_join(events %>% dplyr::select(event_std, event_no), by="event_std")
 
-all_matches <- bind_rows(all_zz_matches, gs_matches, gt_matches)%>%
+all_matches <- bind_rows(all_zz_matches, gs_matches, gt_matches, gts_matches)%>%
   mutate(special_round=ifelse(event_std=="unrecorded", actual_match_no, round))
 
 all_event_rounds <- all_matches  %>%
